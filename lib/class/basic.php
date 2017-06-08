2096fe0b13797aacb2c8d927d0f63f254f576804
zhanghaolong <zhanghaolong@163.com>
2017-06-08 10:49:36 +0800

    photos change

--- lib/class/basic.php
+++ lib/class/basic.php
@@ -751,7 +751,6 @@ class Basic{
                 if (ord($string{$i}) > 127) {
                     if (array_values($ppArr) == $tmpOrd) {
                         break ;
-                        //break 2;
                     }else if(count($ppArr) >= $size) {
                         array_shift($ppArr);
                     }
